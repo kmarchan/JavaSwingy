949a930ba62deb5a2f68b12661a40802075664be
kmarchan <kmarchan@student.wethinkcode.co.za>
2019-07-06 12:51:02 +0200

    add warning to hero creation

--- src/main/java/View/NewGame.java
+++ src/main/java/View/NewGame.java
@@ -3,8 +3,11 @@ package View;
 import Controller.EventData;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class NewGame extends BaseWindow{
     private JButton createHero;
@@ -13,6 +16,8 @@ public class NewGame extends BaseWindow{
     private JPanel newGame;
     private JButton cancel;
     private JEditorPane heroStats;
+    private JLabel heroNameLabel;
+    private JLabel heroTypeSelectorLabel;
     private String[] heroType = {"select type", "Black Mage", "Elf", "Knight", "Orc"};
 
     public NewGame() {
@@ -21,7 +26,13 @@ public class NewGame extends BaseWindow{
         createHero.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!heroNameInput.getText().isEmpty() && heroTypeSelector.getSelectedIndex() != 0) {
+                if (heroNameInput.getText().isEmpty()) {
+                    heroNameLabel.setForeground(Color.RED);
+                }
+                if (heroTypeSelector.getSelectedIndex() == 0) {
+                    heroTypeSelectorLabel.setForeground(Color.RED);
+                }
+                if ((!heroNameInput.getText().isEmpty()) && heroTypeSelector.getSelectedIndex() != 0) {
                     Utils.CharacterFactory.createHero(heroNameInput.getText(), heroType[heroTypeSelector.getSelectedIndex()]);
                 }
             }
@@ -37,10 +48,18 @@ public class NewGame extends BaseWindow{
             public void actionPerformed(ActionEvent e) {
                 if (heroTypeSelector.getSelectedIndex() != 0) {
                     heroStats.setText(heroType[heroTypeSelector.getSelectedIndex()]);
+                    heroTypeSelectorLabel.setForeground(Color.BLACK);
                 }
                 else {
                     heroStats.setText("Please select a Hero Type");
                 }
+            }
+        });
+        heroNameInput.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                heroNameLabel.setForeground(Color.BLACK);
             }
         });
     }
